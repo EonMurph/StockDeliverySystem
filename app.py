65e92f6eed31935fcea677b13023129e165098eb
EonMurph <stiger.home@gmail.com>
2024-02-23 12:29:59 +0000

    implemented basic login and regisration system

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, url_for, redirect
+from flask_session import Session
 from database import get_db, close_db
-from forms import RegistrationForm
-from werkzeug.security import generate_password_hash
+from forms import RegistrationForm, LogInForm
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "this-is-my-secret-key"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 app.teardown_appcontext(close_db)
 
 
 @app.route("/")
 def homepage():
+    if "user_id" not in session:
+        session["user_id"] = None
     return render_template("index.html")
 
 
@@ -39,4 +46,40 @@ def register():
             db.execute(query, (user_id, generate_password_hash(password)))
             db.commit()
 
+            session["user_id"] = user_id
+            return redirect(url_for("homepage"))
+
     return render_template("register.html", form=form, title="Registration")
+
+
+@app.route("/log_in", methods=["GET", "POST"])
+def log_in():
+    form = LogInForm()
+    db = get_db()
+    incorrect_error = ""
+    
+    if form.validate_on_submit():
+        user_id = form.user_id.data
+        password = form.password.data
+        
+        query = """
+                SELECT user_id, password
+                FROM users
+                WHERE user_id = ?;
+                """
+        user_data = db.execute(query, (user_id, )).fetchone()
+        if user_data:
+            if check_password_hash(user_data["password"], password):
+                session["user_id"] = user_id
+                return redirect( url_for("homepage") )
+            
+        else:
+            incorrect_error = "Incorrect username or password."
+    return render_template("log_in.html", form=form, title="Log In", incorrect_error=incorrect_error)
+
+
+@app.route("/log_out")
+def log_out():
+    session["user_id"] = None
+
+    return redirect(url_for("homepage"))
--- forms.py
+++ forms.py
@@ -7,5 +7,12 @@ class RegistrationForm(FlaskForm):
     user_id = StringField("Username: ", validators=[InputRequired()])
     password = PasswordField("Password: ", validators=[InputRequired()])
     password2 = PasswordField("Confirm password: ", validators=[
-                              InputRequired(), EqualTo("password")])
+                              InputRequired(), EqualTo("password", "Your passwords don't match.")])
+    submit = SubmitField("Submit")
+
+
+class LogInForm(FlaskForm):
+    user_id = StringField("Username: ", validators=[InputRequired()])
+    password = PasswordField("Password: ", validators=[InputRequired()])
+
     submit = SubmitField("Submit")
