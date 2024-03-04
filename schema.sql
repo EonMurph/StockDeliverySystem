272dccc9d5769ca798b20ae126b033721fa5cefb
EonMurph <stiger.home@gmail.com>
2024-03-04 14:28:45 +0000

    added admin and manager column to users table

--- schema.sql
+++ schema.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users
 (
-    user_id TEXT PRIMARY KEY,
-    password TEXT NOT NULL
+    user_id INTEGER PRIMARY KEY,
+    password TEXT NOT NULL,
+    admin BOOL NOT NULL DEFAULT '0',
+    manager BOOL NOT NULL DEFAULT '0'
 );
