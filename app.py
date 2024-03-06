b88f39df51cf00ce438c8c1c0c8c2aed1e54afbe
EonMurph <stiger.home@gmail.com>
2024-03-06 21:55:15 +0000

    added code for admins; also added a decorator for restricting access to only admins or managers

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
+import sqlite3  # imported just for `sqlite3.IntegrityError`
 from flask import (Flask,
                    render_template,
-                   session,
-                   url_for, redirect,
-                   g,
-                   request)
+                   url_for, redirect, request,
+                   session, g)
 from flask_session import Session
 from database import get_db, close_db
-from forms import RegistrationForm, LogInForm
+from forms import (RegistrationForm, LogInForm, PermissionsForm)
 from werkzeug.security import generate_password_hash, check_password_hash
-from functools import wraps
+from functools import wraps, partial
 from random import choice
-from pathlib import Path, PurePosixPath
 from os import path
+from glob import glob
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "this-is-my-secret-key"
@@ -23,25 +22,55 @@ app.teardown_appcontext(close_db)
 
 
 @app.before_request
+def init_admin():
+    # TODO see if you can get `before_first_request` working
+    """ # next line found on https://stackoverflow.com/questions/73570041/flask-deprecated-before-first-request-how-to-update
+    app.before_request_funcs[None].remove(init_admin) """
+
+    db = get_db()
+    query = """
+            INSERT INTO users (user_id, password, admin)
+            VALUES (1, ?, 1);
+            """
+    try:
+        db.execute(query, (generate_password_hash("secret"), ))
+        db.commit()
+    except sqlite3.IntegrityError:
+        pass
+
+
+@app.before_request
 def load_logged_in_user():
     g.user = session.get("user_id", None)
+    g.admin = session.get("admin", False)
+    g.manager = session.get("manager", False)
+    g.restrictions = session.get("restrictions", None)
+    g.restricted = session.get("restricted", False)
 
 
-def login_required(view):
+def restricted(view, restriction):
     @wraps(view)
     def wrapped_view(*args, **kwargs):
-        if g.user is None:
-            return redirect(url_for("login", next=request.url))
+        if any([restriction == "admin" and g.admin is False,
+                restriction == "manager" and g.manager is False]):
+            session["restrictions"] = restriction
+            session["restricted"] = True
+
+            return redirect(url_for("log_in", next=request.url))
         return view(*args, **kwargs)
     return wrapped_view
+
+
+# partial function found at https://stackoverflow.com/a/25827070
+admin_required = partial(restricted, restriction="admin")
+manager_required = partial(restricted, restriction="manager")
 
 
 @app.route("/")
 def homepage():
     if g.user:
         return render_template("homepage.html")
-    # tips = [str(path.join(path.basename(path.dirname(file)), path.basename(file))).replace("\\", "/") for file in Path().glob("./templates/tips/*.html")]
-    tips = list(Path().glob("./templates/tips/*.html"))
+    tips = list(glob("./templates/tips/*.html"))
     for i in range(len(tips)):
         parent = path.basename(path.dirname(tips[i]))
         file_name = path.basename(tips[i])
@@ -61,7 +90,6 @@ def register():
         user_id = form.user_id.data
         password = form.password.data
         password2 = form.password2.data
-        
 
         db = get_db()
         query = """
@@ -89,7 +117,6 @@ def register():
 def log_in():
     form = LogInForm()
     db = get_db()
-    incorrect_error = ""
 
     if form.validate_on_submit():
         user_id = form.user_id.data
@@ -102,18 +129,31 @@ def log_in():
                 """
         if user_data := db.execute(query, (user_id,)).fetchone():
             if check_password_hash(user_data["password"], password):
-                    session["user_id"] = user_id
-                    next_page = request.args.get("next") or url_for("homepage")
 
-                    return redirect(next_page)
-            
+                session.clear()
+
+                query = """
+                        SELECT *
+                        FROM users
+                        WHERE user_id = ?;
+                        """
+                user_data = db.execute(query, (user_id,)).fetchone()
+
+                if user_data["admin"] == 1:
+                    session["admin"] = True
+
+                session["user_id"] = user_id
+                next_page = request.args.get("next") or url_for("homepage")
+
+                return redirect(next_page)
+
             else:
-                incorrect_error = "Incorrect password."
+                form.password.errors.append("Incorrect password.")
 
         else:
-            incorrect_error = "Incorrect user id."
+            form.user_id.errors.append("Incorrect user id.")
 
-    return render_template("log_in.html", form=form, incorrect_error=incorrect_error)
+    return render_template("log_in.html", form=form)
 
 
 @app.route("/log_out")
@@ -121,3 +161,40 @@ def log_out():
     session.clear()
 
     return redirect(url_for("homepage"))
+
+
+@app.route("/raise_permissions", methods=["GET", "POST"])
+@admin_required
+def raise_permissions():
+    form = PermissionsForm()
+    message = ''
+
+    query = """
+            SELECT *
+            FROM users
+            WHERE admin = 0
+                AND manager = 0;
+            """
+
+    db = get_db()
+    users = [result["user_id"]
+             for result in db.execute(query).fetchall()]
+
+    form.user_ids.choices = users
+
+    if form.validate_on_submit():
+        user_id = form.user_ids.data
+        permission = "admin" if form.admin_submit.data else "manager"
+
+        print(user_id, permission)
+
+        query = f"""
+                UPDATE users
+                SET {permission} = 1
+                WHERE user_id = ?;
+                """
+        db.execute(query, (user_id, ))
+        db.commit()
+        message = f"User {user_id} was successfully made {permission}."
+
+    return render_template("raise_permissions.html", form=form, message=message)
