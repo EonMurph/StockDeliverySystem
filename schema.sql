55094934f737aafcdfd8c4255a7f760286edfa36
EonMurph <stiger.home@gmail.com>
2024-03-14 09:25:20 +0000

    made columns NOT NULL

--- schema.sql
+++ schema.sql
@@ -15,32 +15,32 @@ CREATE TABLE IF NOT EXISTS users
 CREATE TABLE IF NOT EXISTS products
 (
     product_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    product_name TEXT,
-    product_image TEXT DEFAULT 'default.png'
+    product_name TEXT NOT NULL,
+    product_image TEXT NOT NULL DEFAULT 'default.png'
     
 );
 
 CREATE TABLE IF NOT EXISTS stores
 (
     store_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    manager_id INTEGER,
+    manager_id INTEGER NOT NULL,
     FOREIGN KEY (manager_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS product_in_stores
 (
-    product_id INTERGER
+    product_id INTERGER NOT NULL,
     order_point INTEGER DEFAULT 10,
-    store_id INTEGER,
-    amount INTEGER,
+    store_id INTEGER NOT NULL,
+    amount INTEGER NOT NULL,
     FOREIGN KEY (product_id) REFERENCES products(product_id)
     FOREIGN KEY (store_id) REFERENCES stores(store_id)
 );
 
 CREATE TABLE IF NOT EXISTS employees
 (
-    store_id INTEGER,
-    employee_id INTEGER,
+    store_id INTEGER NOT NULL,
+    employee_id INTEGER NOT NULL,
     FOREIGN KEY (store_id) REFERENCES stores(store_id),
     FOREIGN KEY (employee_id) REFERENCES users(user_id)
 );
