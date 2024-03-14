4c2c02d6a5e6cbcb1df5d4799036c9a6183131e4
EonMurph <stiger.home@gmail.com>
2024-03-14 17:50:37 +0000

    got rid of toggle register/login on homepage

--- app.py
+++ app.py
@@ -73,112 +73,109 @@ def homepage():
     if g.user:
         return render_template("homepage.html")
 
-    reg_form = RegistrationForm()
+    else:
+        return redirect("register")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
     db = get_db()
     query = "SELECT store_id from stores;"
-    reg_form.store_id.choices = [result["store_id"]
-                                 for result in db.execute(query).fetchall()]
-    login_form = LogInForm()
+    form.store_id.choices = [result["store_id"]
+                             for result in db.execute(query).fetchall()]
 
     tips = list(glob("./templates/tips/*.html"))
     for i in range(len(tips)):
         parent = path.basename(path.dirname(tips[i]))
         file_name = path.basename(tips[i])
         tips[i] = str(path.join(parent, file_name)).replace("\\", "/")
-
     # TODO populate tips and remove next line
     index = choice(tips) if tips else "index.html"
 
-    if reg_form.validate_on_submit():
-        reg_form = register(reg_form)
-        if session.get("user_id", None):
-            return redirect(url_for("homepage"))
-    if login_form.validate_on_submit():
-        login_form = log_in(login_form)
-        if session.get("user_id", None):
-            next_page = request.args.get("next") or url_for("homepage")
-            return redirect(next_page)
+    if form.validate_on_submit():
+        user_id = form.user_id.data
+        password = form.password.data
+        password2 = form.password2.data
+        store_id = form.store_id.data
 
-    return render_template(index, reg_form=reg_form, login_form=login_form)
-
-
-def register(form):
-    user_id = form.user_id.data
-    password = form.password.data
-    password2 = form.password2.data
-    store_id = form.store_id.data
-
-    db = get_db()
-    query = """
-            SELECT *
-            FROM users
-            WHERE user_id = ?;
-            """
-    if db.execute(query, (user_id,)).fetchone():
-        form.user_id.errors.append("Username already taken.")
-    else:
+        db = get_db()
         query = """
-                INSERT INTO users (user_id, password)
-                VALUES (?, ?)
+                SELECT *
+                FROM users
+                WHERE user_id = ?;
                 """
-        db.execute(query, (user_id, generate_password_hash(password)))
-
-        query = """
-                INSERT INTO employees (store_id, employee_id)
-                VALUES (?, ?);
-                """
-        db.execute(query, (store_id, user_id))
-
-        db.commit()
-
-        session["user_id"] = user_id
-        session["store_id"] = store_id
-
-    return form
-
-
-def log_in(form):
-    user_id = form.user_id.data
-    password = form.password.data
-
-    db = get_db()
-    query = """
-            SELECT user_id, password
-            FROM users
-            WHERE user_id = ?;
-            """
-    if user_data := db.execute(query, (user_id,)).fetchone():
-        if check_password_hash(user_data["password"], password):
-
-            session.clear()
+        if db.execute(query, (user_id,)).fetchone():
+            form.user_id.errors.append("Username already taken.")
+        else:
+            query = """
+                    INSERT INTO users (user_id, password)
+                    VALUES (?, ?)
+                    """
+            db.execute(query, (user_id, generate_password_hash(password)))
 
             query = """
-                    SELECT *
-                    FROM users
-                    WHERE user_id = ?;
+                    INSERT INTO employees (store_id, employee_id)
+                    VALUES (?, ?);
                     """
-            user_data = db.execute(query, (user_id,)).fetchone()
+            db.execute(query, (store_id, user_id))
 
-            if user_data["admin"] == 1:
-                session["admin"] = True
-            else:
-                query = """
-                        SELECT store_id
-                        FROM employees
-                        WHERE employee_id = ?"""
-                store_id = db.execute(query, (user_id, )).fetchone()[
-                    "store_id"]
-                session["store_id"] = store_id
+            db.commit()
 
             session["user_id"] = user_id
+            session["store_id"] = store_id
+
+    return render_template(index, form=form)
+
+
+@app.route("/log_in", methods=["GET", "POST"])
+def log_in():
+    form = LogInForm()
+
+    if form.validate_on_submit():
+        user_id = form.user_id.data
+        password = form.password.data
+
+        db = get_db()
+        query = """
+                SELECT user_id, password
+                FROM users
+                WHERE user_id = ?;
+                """
+        if user_data := db.execute(query, (user_id,)).fetchone():
+            if check_password_hash(user_data["password"], password):
+
+                session.clear()
+
+                query = """
+                        SELECT *
+                        FROM users
+                        WHERE user_id = ?;
+                        """
+                user_data = db.execute(query, (user_id,)).fetchone()
+
+                if user_data["admin"] == 1:
+                    session["admin"] = True
+                else:
+                    query = """
+                            SELECT store_id
+                            FROM employees
+                            WHERE employee_id = ?"""
+                    store_id = db.execute(query, (user_id, )).fetchone()[
+                        "store_id"]
+                    session["store_id"] = store_id
+
+                session["user_id"] = user_id
+                next_page = request.args.get("next") or url_for("homepage")
+                return redirect(next_page)
+
+            else:
+                form.password.errors.append("Incorrect password.")
 
         else:
-            form.password.errors.append("Incorrect password.")
+            form.user_id.errors.append("Incorrect user id.")
 
-    else:
-        form.user_id.errors.append("Incorrect user id.")
-
-    return form
+    return render_template("log_in.html", form=form)
 
 
 @app.route("/log_out")
